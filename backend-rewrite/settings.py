b454fd269d64086c45ea802c987c5401875d7d61
Rodriguez Breil S <rbsoenoto@gmail.com>
2022-10-28 20:22:20 +0700

    fix heroku 2

--- backend-rewrite/settings.py
+++ backend-rewrite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-IS_HEROKU = "DYNO" in os.environ
+IS_HEROKU = "DYNO_RAM" in os.environ
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'SET SECRET FROM OS ENVIRONMENT'
